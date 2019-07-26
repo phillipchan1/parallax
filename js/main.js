475468d9fdf6667dddff95c2d13de68d602398ce
Phil Chan <phillipchan1@gmail.com>
2019-07-26 15:18:09 -0700

    Begin prototyping

--- js/main.js
+++ js/main.js
@@ -1,10 +1,36 @@
 var parallax = (function() {
     var paralaxElements = document.querySelectorAll('.parallax')
 
-    window.addEventListener('scroll', () => {
-        const scrolled = window.pageYOffset
-        console.log('TCL: parallax -> scrolled', scrolled)
+    // determines if an element is in the viewport
+    var isInViewport = function(el) {
+        var r, html
+        if (!el || 1 !== el.nodeType) {
+            return false
+        }
+        html = document.documentElement
+        r = el.getBoundingClientRect()
 
-        paralaxElements.forEach(el => {})
+        return (
+            !!r &&
+            r.bottom >= 0 &&
+            r.right >= 0 &&
+            r.top <= html.clientHeight &&
+            r.left <= html.clientWidth
+        )
+    }
+
+    window.addEventListener('scroll', () => {
+        paralaxElements.forEach((el, i) => {
+            if (!isInViewport(el)) {
+                return
+            }
+
+            console.log()
+
+            el.style.backgroundPosition = `0 -${el.getBoundingClientRect().top /
+                2}px `
+
+            console.log(`picture ${i + 1} in viewport!`)
+        })
     })
 })()
