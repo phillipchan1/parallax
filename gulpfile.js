85cd76d4c2da7c81a4227ecf79d594b69bd5f187
phillipchan1 <phillipchan1@gmail.com>
2019-07-29 11:53:08 -0700

    Move example to example/ folder

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,15 @@
-var gulp = require('gulp');
-var server = require('gulp-server-livereload');
+var gulp = require('gulp')
+var server = require('gulp-server-livereload')
 
 gulp.task('webserver', function() {
-	gulp.src('./')
-	.pipe(server({
-		livereload: true,
-		defaultFile: 'index.html',
-		directoryListing: false,
-		open: true
-	}));
-});
+  gulp.src('./').pipe(
+    server({
+      livereload: true,
+      defaultFile: '/example/index.html',
+      directoryListing: false,
+      open: true
+    })
+  )
+})
 
-gulp.task('default', ['webserver']);
+gulp.task('default', ['webserver'])
