27dd52bef04e555cee710cb21ac9b3d045495e53
phillipchan1 <phillipchan1@gmail.com>
2019-08-25 15:48:03 -0700

    Fixed bug where it would give negative adjustment values

--- src/config.ts
+++ src/config.ts
@@ -3,7 +3,7 @@ import { Config } from './types/Config'
 export const config: Config = {
   debugMode: false,
   speedRatio: 0.3,
-  pixelsOverflowing: 500,
+  pixelsOverflowing: 250,
   defaultXPosition: '50%',
   pageOffsetToInitialize: 200,
   selector: '.parallax'
