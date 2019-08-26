cc9c7e392d97a97a504c1b6069492cf9221eb024
Phil Chan <phillipchan1@gmail.com>
2019-08-26 09:02:40 -0700

    Revert default pixelsOverflowing

--- src/config.ts
+++ src/config.ts
@@ -3,7 +3,7 @@ import { Config } from './types/Config'
 export const config: Config = {
   debugMode: false,
   speedRatio: 0.3,
-  pixelsOverflowing: 250,
+  pixelsOverflowing: 500,
   defaultXPosition: '50%',
   pageOffsetToInitialize: 200,
   selector: '.parallax'
