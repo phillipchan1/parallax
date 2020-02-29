fb5fe7123ba2334b3312ddb1d999dcff4e6138cf
Phil <phillipchan1@gmail.com>
2020-02-29 08:26:02 -0800

    Enlarge speed tapering

--- src/lib/getSpeedLimit.ts
+++ src/lib/getSpeedLimit.ts
@@ -1,10 +1,19 @@
+const maxSpeed = 2
+const minSpeed = 0.01
+
+/**
+ * This method functions as a speed gate to keep
+ * from user error specifying speeds that are
+ * astronomically fast or slow
+ * @param speed
+ */
 export const getSpeedLimit = (speed: number): number => {
-  if (speed < 0.1) {
-    return 0.1
+  if (speed < minSpeed) {
+    return minSpeed
   }
 
-  if (speed > 0.5) {
-    return 0.5
+  if (speed > maxSpeed) {
+    return maxSpeed
   }
 
   return speed
