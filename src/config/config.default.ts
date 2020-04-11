d975e65357e6a51d526ce436e9c254001a001466
Phillip Chan <phil.chan@sce.com>
2020-04-11 15:43:18 -0700

    Put the docstrings on the correct file...

--- src/config/config.default.ts
+++ src/config/config.default.ts
@@ -1,34 +1,9 @@
 import { Config } from './config.types'
 
 export const defaultConfig: Config = {
-  /**
-   * Puts out console statements in debug
-   * mode
-   */
   debugMode: false,
-
-  /**
-   * Should only be a value between 1-10
-   */
-  speedRatio: 1,
-
-  /**
-   * The wrapper around the parallax and normal items
-   * Expects a DOM hierarchy of something like
-   * <div class="wrapper">
-   *  <div class="parallax"></div>
-   * </div>
-   */
+  speed: 1,
   wrapperClassName: 'wrapper',
-
-  /**
-   * The class of the parallax examples
-   */
   parallaxClassName: 'parallax',
-
-  /**
-   * If parallax item is not given a height,
-   * this is the fallback height
-   */
   defaultHeight: '70vh'
 }
--- src/config/config.types.ts
+++ src/config/config.types.ts
@@ -1,7 +1,32 @@
 export type Config = {
+  /**
+   * Puts out console statements in debug
+   * mode
+   */
   debugMode?: boolean
+
+  /**
+   * If parallax item is not given a height,
+   * this is the fallback height
+   */
   defaultHeight: string
-  speedRatio: number
+
+  /**
+   * Should only be a value between 1-10
+   */
+  speed: number
+
+  /**
+   * The wrapper around the parallax and normal items
+   * Expects a DOM hierarchy of something like
+   * <div class="wrapper">
+   *  <div class="parallax"></div>
+   * </div>
+   */
   wrapperClassName: string
+
+  /**
+   * The class of the parallax examples
+   */
   parallaxClassName: string
 }
